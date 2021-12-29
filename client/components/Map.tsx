a674db604ac8861f6b3ee33747338c1983769c03
M.D. van Es <es@mdworld.nl>
2021-12-29 18:12:06 +0100

    Show polyline

--- client/components/Map.tsx
+++ client/components/Map.tsx
@@ -1,25 +1,50 @@
 import { FC, useEffect, useState } from "react";
-import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
+import {
+  MapContainer,
+  TileLayer,
+  Marker,
+  Popup,
+  Polyline,
+} from "react-leaflet";
+
+interface Item {
+  loc: [number, number];
+  time: string;
+}
 
 const getCoords = async () => {
   const result = await fetch("/api/coords");
-  const json = await result.json();
+  const json: { data: Item[] } = await result.json();
   //   console.log(json);
   //   setCoords(json);
   return json.data;
 };
 
 const Map: FC = () => {
-  const [coords, setCoords] = useState([]);
+  const [coords, setCoords] = useState<Item[]>([]);
+  const [center, setCenter] = useState<[number, number]>([52, 5.1]);
 
   useEffect(() => {
-    getCoords().then((x) => setCoords(x));
+    getCoords().then((result) => {
+      setCoords(result);
+      //   if (result.length > 0) {
+      //     setCenter(result[0].loc);
+      //     console.log(result[0].loc);
+      //   }
+    });
   }, []);
+
+  useEffect(() => {
+    if (coords.length > 0) {
+      setCenter(coords[0].loc);
+      console.log(coords[0].loc);
+    }
+  }, [coords]);
 
   return (
     <div>
       <MapContainer
-        center={[52.062763, 4.495023]}
+        center={center}
         zoom={20}
         //   scrollWheelZoom={false}
       >
@@ -27,20 +52,21 @@ const Map: FC = () => {
           attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
           url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
         />
-        <Marker position={[52.062763, 4.495023]}>
+        {/* <Marker position={[1, 1]}>
           <Popup>
             A pretty CSS3 popup. <br /> Easily customizable.
           </Popup>
-        </Marker>
-        {coords.map((coord: any, index) => {
+        </Marker> */}
+        {/* {coords.map((coord, index) => {
           //   console.log(coord);
           return (
             <Marker
-              key={`${coord.lat}-${coord.lon}-${index}`}
-              position={[coord.lat, coord.lon]}
+              key={coord.time}
+              position={coord.loc}
             ></Marker>
           );
-        })}
+        })} */}
+        <Polyline positions={coords.map((coord) => coord.loc)} />
       </MapContainer>
     </div>
   );
--- client/pages/api/coords.ts
+++ client/pages/api/coords.ts
@@ -1,44 +1,54 @@
-// TODO this just works by navigating to localhost:3000/api/lora
+// NOTE this just works by navigating to localhost:3000/api/coords
 
 import { IncomingMessage, ServerResponse } from "http";
 import { NextResponse } from "next/server";
 import { readFileSync } from "fs";
 
-// interface Line {
-//   n: string;
-//   v: number;
-//   vs: string;
-// }
+interface Item {
+  loc: [number, number];
+  time: string;
+}
+
+const parseLogLine = (line: string): Item | null => {
+  // console.log(line);
+  const [date, time, coordsStr] = line.split(" ");
+
+  if (coordsStr) {
+    const [latStr, lonStr] = coordsStr.split(",");
+    const lat = parseFloat(latStr);
+    const lon = parseFloat(lonStr);
+    if(isNaN(lat) || isNaN(lon)) {
+      return null;
+    }
+    try {
+      const loc: [number, number] = [lat, lon];
+      return {
+        loc,
+        time: `${date} ${time}`,
+      };
+    } catch (err) {
+      return null;
+    }
+  } else {
+    return null;
+  }
+};
+
+const isNotNull = <T>(item: T | null): item is T => {
+  return item !== null;
+};
 
 export default function handler(req: IncomingMessage, res: any) {
   if (req.method === "POST") {
     res.status(200).json({ status: "POST OK" });
   } else {
-    // @ts-expect-error req.query exists
-    console.log("req.query", req.query);
-
     // const result = readFileSync("./log.txt", "utf8");
     const result = readFileSync("../server/log.txt", "utf8");
     // console.log(result);
 
     const lines = result.split("\n");
-    const coords = lines.map((line) => {
-      const [, , coordsStr] = line.split(" ");
-
-      if(coordsStr) {
-        const [lat, lon] = coordsStr.split(",");
-        return {
-          lat,
-          lon,
-        };
-      } else {
-        return {
-          lat: 0,
-          lon: 0,
-        }
-      }
-    });
-    console.log(coords);
+    const coords = lines.map<Item | null>(parseLogLine).filter(isNotNull);
+    // console.log(coords);
 
     res.status(200).json({ status: "GET OK", data: coords });
   }

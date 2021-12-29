c339a61e483022ab27698b26818e0329726c348e
M.D. van Es <es@mdworld.nl>
2021-12-29 18:30:07 +0100

    Fix map centering

--- client/components/Map.tsx
+++ client/components/Map.tsx
@@ -1,57 +1,28 @@
-import { FC, useEffect, useState } from "react";
+import { LatLngTuple } from "leaflet";
+import { FC } from "react";
 import {
   MapContainer,
   TileLayer,
   Marker,
   Popup,
   Polyline,
+  useMap,
 } from "react-leaflet";
-
-interface Item {
-  loc: [number, number];
-  time: string;
-}
-
-const getCoords = async () => {
-  const result = await fetch("/api/coords");
-  const json: { data: Item[] } = await result.json();
-  //   console.log(json);
-  //   setCoords(json);
-  return json.data;
-};
+import MapContent from "./MapContent";
 
 const Map: FC = () => {
-  const [coords, setCoords] = useState<Item[]>([]);
-  const [center, setCenter] = useState<[number, number]>([52, 5.1]);
-
-  useEffect(() => {
-    getCoords().then((result) => {
-      setCoords(result);
-      //   if (result.length > 0) {
-      //     setCenter(result[0].loc);
-      //     console.log(result[0].loc);
-      //   }
-    });
-  }, []);
-
-  useEffect(() => {
-    if (coords.length > 0) {
-      setCenter(coords[0].loc);
-      console.log(coords[0].loc);
-    }
-  }, [coords]);
-
   return (
     <div>
       <MapContainer
-        center={center}
+        // center={DEFAULT_CENTER}
         zoom={20}
         //   scrollWheelZoom={false}
       >
-        <TileLayer
+        <MapContent />
+        {/* <TileLayer
           attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
           url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-        />
+        /> */}
         {/* <Marker position={[1, 1]}>
           <Popup>
             A pretty CSS3 popup. <br /> Easily customizable.
@@ -66,7 +37,7 @@ const Map: FC = () => {
             ></Marker>
           );
         })} */}
-        <Polyline positions={coords.map((coord) => coord.loc)} />
+        {/* <Polyline positions={coords.map((coord) => coord.loc)} /> */}
       </MapContainer>
     </div>
   );
